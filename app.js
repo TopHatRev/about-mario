e6c09ca79995c22a9ca1773af7036163dcdcf568
Dylan Blackwell <tophatrev@gmail.com>
2023-01-25 15:41:28 +0000

    Add functions.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ if (question1 == true) {
   alert("Ah well.");
 }
 
+function questionColour() {
 let colour = prompt("What is my favourite colour?");
 
 switch (colour.toLowerCase()) {
@@ -23,30 +24,52 @@ switch (colour.toLowerCase()) {
   default:
     alert("Pretty sure that colour doesn't exist. Have you thought about red? ");
 }
+}
+
+function questionFootball(){
 let question2 = confirm("What do you think , do I like football?");
 if (question2 == true) {
   alert("Great, you get to know me!");} else {
     alert("Wasn`t expecting that!!! ")
   }
+}
+
+  function questionLFC(){
   let question3 = confirm("I live in Liverpool , what do you think , do I follow  Liverpool FC ?"); if (question3 == true) {
     alert("Up the Reds!!!");
   } else {
     alert("Ohhh noo , i`m a big fan of them  ");
   }
+}
+
+  function questionAnfield(){
   let question4 = confirm("Have you ever been on Anfield?"); if (question4 == true ) { 
     alert("Awesome!!!");
   } else {
     alert("You should go at least once!");
   }
+}
+
+  function questionMindGame(){
   let question5 = confirm("Do you wanna play a mind game?");if (question4 == true ) { 
     alert("Good stuff !!!");
   } else {
     alert("You should give it a go!");
   }
+}
   
+function questionNumbers(){
   let question6 = prompt("I`m thinking of a number between 1 and 100 , can you guess that from 4 attempts? You cand start now with your first attempt!");
-  let myNumber = 25 ;
-  
-  
+  let myNumber = 25;
+}
+  questionColour();
 
-  
+  questionFootball();
+
+  questionLFC();
+
+  questionAnfield();
+
+  questionMindGame();
+
+  questionNumbers();
