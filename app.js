2d2212aeddc5a60de01e5918c86666c7ac2a48b9
Dylan Blackwell <tophatrev@gmail.com>
2023-01-25 14:35:33 +0000

    Spellcheck.

--- app.js
+++ app.js
@@ -3,48 +3,48 @@
 
 
 
-let question1 = confirm("Do you want to take part in a small quizz?");
+let question1 = confirm("Do you want to take part in a small quiz?");
 
 if (question1 == true) {
-  alert("Let`s get down to bussines");
+  alert("Let`s get down to business.");
 } else {
   alert("Ah well.");
 }
 
-let colour = prompt("What is my  favourite colour?");
+let colour = prompt("What is my favourite colour?");
 
 switch (colour.toLowerCase()) {
   case "red":
-    alert("My  favourite colour is  red. Great.");
+    alert("My favourite colour is red. Great.");
     break;
   case "green":
-    alert("Nice. But i prefer red ");
+    alert("Nice. But I prefer red ");
     break;
   default:
-    alert("Pretty sure that colour doesn't exist. Have you thought about red ? ");
+    alert("Pretty sure that colour doesn't exist. Have you thought about red? ");
 }
-let question2 = confirm("What do you think , do i like football?");
+let question2 = confirm("What do you think , do I like football?");
 if (question2 == true) {
-  alert("Great , you get to know me!");} else {
+  alert("Great, you get to know me!");} else {
     alert("Wasn`t expecting that!!! ")
   }
-  let question3 = confirm("I live in Liverpool , what do you think , do i follow  Liverpool Fc ?"); if (question3 == true) {
+  let question3 = confirm("I live in Liverpool , what do you think , do I follow  Liverpool FC ?"); if (question3 == true) {
     alert("Up the Reds!!!");
   } else {
     alert("Ohhh noo , i`m a big fan of them  ");
   }
-  let question4 = confirm("Have you ever been on Anfield ?"); if (question4 == true ) { 
+  let question4 = confirm("Have you ever been on Anfield?"); if (question4 == true ) { 
     alert("Awesome!!!");
   } else {
-    alert("You should go at least once !");
+    alert("You should go at least once!");
   }
   let question5 = confirm("Do you wanna play a mind game?");if (question4 == true ) { 
     alert("Good stuff !!!");
   } else {
-    alert("You should give it a go !");
+    alert("You should give it a go!");
   }
   
-  let question6 = prompt("I`m thinking of a number between 1 and 100 , can you guess that from 4 attempts?You cand start now with your first attempt !");
+  let question6 = prompt("I`m thinking of a number between 1 and 100 , can you guess that from 4 attempts? You cand start now with your first attempt!");
   let myNumber = 25 ;
   
   
